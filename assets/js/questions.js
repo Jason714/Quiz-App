00dfd9b4bfdf8d65a2776d7455d6ca42683391c9
Jason Blake <jblakecodes@gmail.com>
2019-12-28 12:31:27 -0800

    questions array populated

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,12 +1,52 @@
 var questions = [
   {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    title: "What is the name of Jon's direwolf?",
+    choices: ["Summer", "Grey Wind", "Ghost", "Nymeria"],
+    answer: "Ghost"
   },
   {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    title: "Which name is given to the bastards of The Westerlands?",
+    choices: ["Stone", "Rivers", "Snow", "Hill"],
+    answer: "Hill"
+  }
+  {
+    title: "What does Varys usually claim to be his aim?",
+    choices: ["Serve the realm", "Avenge the death of Rhaegar Targaryan", "Serve the Targaryans", "Avenge the sack of King's Landing"],
+    answer: "Serve the realm"
+  },
+  {
+    title: "Which chemical was used during the Battle of the Blackwater to destroy Stannis Baratheon's fleet?",
+    choices: ["Milk of the Poppy", "The Strangler", "Essence of Nightshade", "Wildfire"],
+    answer: "Wildfire"
+  }
+  {
+    title: "What is the nickname given to Brynden Tully?",
+    choices: ["Kingslayer", "Blackfish", "Black Dread", "The Conqueror"],
+    answer: "Blackfish"
+  },
+  {
+    title: "Who created the first White Walker?",
+    choices: ["The Children of the Forest", "the Three-eyed Raven", "The Night's Watch", "Bran Stark"],
+    answer: "The Children of the Forest"
+  }
+  {
+    title: "Shae meets Tyrion for the first time in a Lannister army's camp. Where?",
+    choices: ["Kingswood", "The North", "Volantis", "The Riverlands"],
+    answer: "The Riverlands"
+  },
+  {
+    title: "Who is king of Westeros when the the series begins?",
+    choices: ["Aerys Targaryan", "Eddard Stark", "Robert Baratheon", "Tywin Lannister"],
+    answer: "Robert Baratheon"
+  }
+  {
+    title: "Who is Joffrey, Myrcella and Tommen Baratheon's real father?",
+    choices: ["Jaime Lannister", "Renly Baratheon", "Eddard Stark", "Robert Baratheon"],
+    answer: "Jaime Lannister"
+  },
+  {
+    title: "What house has a sigil of a silver trout?",
+    choices: ["Hoouse Greyjoy", "House Tully", "House Tyrell", "House Lannister"],
+    answer: "House Tully"
   }
 ];
