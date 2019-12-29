3d6c3027ca48acea827551f12016eb326b914997
Jason Blake <jblakecodes@gmail.com>
2019-12-28 19:23:18 -0800

    added msg div to HTML, updated css style for msg div, created js functions to load random questions, created timer function, created end quiz function to display end-container

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,52 +1,94 @@
 var questions = [
   {
     title: "What is the name of Jon's direwolf?",
-    choices: ["Summer", "Grey Wind", "Ghost", "Nymeria"],
-    answer: "Ghost"
+    answers: [
+      { text: "Summer", correct: false },
+      { text: "Grey Wind", correct: false },
+      { text: "Ghost", correct: true },
+      { text: "Nymeria", correct: false }
+    ]
   },
   {
     title: "Which name is given to the bastards of The Westerlands?",
-    choices: ["Stone", "Rivers", "Snow", "Hill"],
-    answer: "Hill"
+    answers: [
+      { text: "Stone", correct: false },
+      { text: "Rivers", correct: false },
+      { text: "Snow", correct: false },
+      { text: "Hill", correct: true }
+    ]
   },
   {
     title: "What does Varys usually claim to be his aim?",
-    choices: ["Serve the realm", "Avenge the death of Rhaegar Targaryan", "Serve the Targaryans", "Avenge the sack of King's Landing"],
-    answer: "Serve the realm"
+    answers: [
+      { text: "Serve the realm", correct: true },
+      { text: "Avenge the death of Rhaegar Targaryan", correct: false },
+      { text: "Serve the Targaryans", correct: false },
+      { text: "Avenge the sack of King's Landing", correct: false }
+    ]
   },
   {
-    title: "Which chemical was used during the Battle of the Blackwater to destroy Stannis Baratheon's fleet?",
-    choices: ["Milk of the Poppy", "The Strangler", "Essence of Nightshade", "Wildfire"],
-    answer: "Wildfire"
-  }
+    title:
+      "Which chemical was used during the Battle of the Blackwater to destroy Stannis Baratheon's fleet?",
+    answers: [
+      { text: "Milk of the Poppy", correct: false },
+      { text: "The Strangler", correct: false },
+      { text: "Essence of Nightshade", correct: false },
+      { text: "Wildfire", correct: true }
+    ]
+  },
   {
     title: "What is the nickname given to Brynden Tully?",
-    choices: ["Kingslayer", "Blackfish", "Black Dread", "The Conqueror"],
-    answer: "Blackfish"
+    answers: [
+      { text: "Black Dread", correct: false },
+      { text: "Blackfish", correct: true },
+      { text: "The Conqueror", correct: false },
+      { text: "Kingslayer", correct: false }
+    ]
   },
   {
     title: "Who created the first White Walker?",
-    choices: ["The Children of the Forest", "the Three-eyed Raven", "The Night's Watch", "Bran Stark"],
-    answer: "The Children of the Forest"
-  }
+    answers: [
+      { text: "The Children of the Forest", correct: true },
+      { text: "The Three-eyed Raven", correct: false },
+      { text: "The Night's Watch", correct: false },
+      { text: "Bran Stark", correct: false }
+    ]
+  },
   {
-    title: "Shae meets Tyrion for the first time in a Lannister army's camp. Where?",
-    choices: ["Kingswood", "The North", "Volantis", "The Riverlands"],
-    answer: "The Riverlands"
+    title:
+      "Shae meets Tyrion for the first time in a Lannister army's camp. Where?",
+    answers: [
+      { text: "Kingswood", correct: false },
+      { text: "The North", correct: false },
+      { text: "Volantis", correct: false },
+      { text: "The Riverlands", correct: true }
+    ]
   },
   {
     title: "Who is king of Westeros when the the series begins?",
-    choices: ["Aerys Targaryan", "Eddard Stark", "Robert Baratheon", "Tywin Lannister"],
-    answer: "Robert Baratheon"
-  }
+    answers: [
+      { text: "Aerys Targaryan", correct: false },
+      { text: "Eddard Stark", correct: false },
+      { text: "Robert Baratheon", correct: true },
+      { text: "Tywin Lannister", correct: false }
+    ]
+  },
   {
     title: "Who is Joffrey, Myrcella and Tommen Baratheon's real father?",
-    choices: ["Jaime Lannister", "Renly Baratheon", "Eddard Stark", "Robert Baratheon"],
-    answer: "Jaime Lannister"
+    answers: [
+      { text: "Jamie Lannister", correct: true },
+      { text: "Renly Baratheon", correct: false },
+      { text: "Eddard Stark", correct: false },
+      { text: "Robert Baratheon", correct: false }
+    ]
   },
   {
     title: "What house has a sigil of a silver trout?",
-    choices: ["Hoouse Greyjoy", "House Tully", "House Tyrell", "House Lannister"],
-    answer: "House Tully"
+    answers: [
+      { text: "House Greyjoy", correct: false },
+      { text: "House Tully", correct: true },
+      { text: "House Tyrell", correct: false },
+      { text: "House Lannister", correct: false }
+    ]
   }
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,128 @@ var timerEl = document.querySelector("#time");
 var questionEl = document.querySelector("#question");
 var answersEl = document.querySelector("#answers");
 var submitEl = document.querySelector("#submit-btn");
-
+var msgEl = document.querySelector("#msg");
+// Set global variables
+var shuffledQuestions, currentQuestionIndex;
+var time = questions.length * 16;
+var timer;
+// Listener for start button
 startBtnEl.addEventListener("click", startQuiz);
-
+// Function to start the quiz
 function startQuiz() {
+  // Hide start page
   startPageEl.classList.add("hidden");
+  // Make questions appear in random order
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  // Set question index
+  currentQuestionIndex = 0;
+  // Display question
   questionContainerEl.classList.remove("hidden");
+  // Start the timer element
+  timer = setInterval(timerCount, 1000);
+  // Show the time remaining
+  timerEl.textContent = time;
+  // Call the setNextQuestion function
+  setNextQuestion();
+}
+// Function to get the page ready to display the next question
+function setNextQuestion() {
+  // Call the resetState function
+  resetState();
+
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(title) {
+  questionEl.innerText = title.title;
+  title.answers.forEach(answer => {
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    button.classList.add("ans-btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectedAnswer);
+    answersEl.appendChild(button);
+  });
+}
+
+function timerCount() {
+  // Update the timer count and display the new time
+  time--;
+  timerEl.textContent = time;
+  // End the game if the user runs out of time
+  if (time <= 0) {
+    endQuiz();
+  }
+}
+
+function resetState() {
+  while (answersEl.firstChild) {
+    answersEl.removeChild(answersEl.firstChild);
+    msgEl.classList.remove("hidden");
+  }
+}
+
+function selectedAnswer(e) {
+  var slectedBtn = e.target;
+  Array.from(answersEl.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct);
+  });
+
+  // Statement to check for wrong answer
+  if (slectedBtn.classList.contains("wrong")) {
+    // Time penalty for wrong answer
+    time -= 15;
+    if (time < 0) {
+      time = 0;
+    }
+    // Update the timer
+    timerEl.textContent = time;
+
+    msgEl.classList.add("wrong");
+    msgEl.innerText = "Sorry, that answer was incorrect!";
+  } else {
+    msgEl.classList.add("correct");
+    msgEl.innerText = "Yes, that is the correct answer!";
+  }
+  setTimeout(function() {
+    msgEl.classList.add("hidden");
+  }, 1500);
+  // Go to next question
+  currentQuestionIndex++;
+  // Check if we've run out of questions
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    setNextQuestion();
+  } else {
+    endQuiz();
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+function endQuiz() {
+  // Stop the timer
+  clearInterval(timer);
+  // Go to the game over screen
+  var endQuizEl = document.querySelector("#end-container");
+  endQuizEl.classList.remove("hidden");
+  // Display the users final score
+  var userScore = document.querySelector("#score");
+  userScore.textContent = time;
+  // Hide the questions and answers
+  questionContainerEl.classList.add("hidden");
 }
